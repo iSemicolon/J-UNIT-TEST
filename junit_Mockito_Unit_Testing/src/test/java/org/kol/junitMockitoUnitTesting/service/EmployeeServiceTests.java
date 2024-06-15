71d782d34c3e1eebddeed05bfdcdf27c69894832
iSemicolon <palash.samanta7497@gmail.com>
2024-06-16 00:28:14 +0530

    " Junit for Exception handling"

--- junit_Mockito_Unit_Testing/src/test/java/org/kol/junitMockitoUnitTesting/service/EmployeeServiceTests.java
+++ junit_Mockito_Unit_Testing/src/test/java/org/kol/junitMockitoUnitTesting/service/EmployeeServiceTests.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kol.junitMockitoUnitTesting.exception.ResourceNotFoundException;
 import org.kol.junitMockitoUnitTesting.model.Employee;
 import org.kol.junitMockitoUnitTesting.repository.EmployeeRepository;
 import org.kol.junitMockitoUnitTesting.service.impl.EmployeeServiceImpl;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -67,6 +72,26 @@ public class EmployeeServiceTests {
 
         //then- output
         Assertions.assertThat(savedEmployee).isNotNull();
+
+    }
+
+
+    @DisplayName("If employee email already exists then Throws Exception ")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+
+        //given- precondition or setup
+
+        given(employeeRepository.findByEmail(employee1.getEmail())).willReturn(Optional.of(employee1));
+
+        //when- behaviour or function
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+            Employee savedEmployee=employeeServiceImpl.saveEmployee(employee1);
+        });
+
+
+        //then- output
+        verify(employeeRepository, Mockito.never()).save(any(Employee.class));
 
     }
 }
