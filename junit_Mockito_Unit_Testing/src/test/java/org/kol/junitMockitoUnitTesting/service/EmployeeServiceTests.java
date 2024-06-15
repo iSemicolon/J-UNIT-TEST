6eccb351ec901fdf582569ac0b61486e015ca40b
iSemicolon <palash.samanta7497@gmail.com>
2024-06-16 00:06:47 +0530

    "Introduced Mockito framework , @Mock and @InjectsMock"

--- junit_Mockito_Unit_Testing/src/test/java/org/kol/junitMockitoUnitTesting/service/EmployeeServiceTests.java
+++ junit_Mockito_Unit_Testing/src/test/java/org/kol/junitMockitoUnitTesting/service/EmployeeServiceTests.java
@@ -4,25 +4,41 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.kol.junitMockitoUnitTesting.model.Employee;
 import org.kol.junitMockitoUnitTesting.repository.EmployeeRepository;
 import org.kol.junitMockitoUnitTesting.service.impl.EmployeeServiceImpl;
-import org.mockito.BDDMockito;
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-//@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
 
+    @Mock
     EmployeeRepository employeeRepository;
-    EmployeeService employeeService;
+
+    @InjectMocks
+    EmployeeServiceImpl employeeServiceImpl;
+
     Employee employee1;
 
     @BeforeEach
     public void setup() {
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        employeeService = new EmployeeServiceImpl(employeeRepository);
+       // employeeRepository = Mockito.mock(EmployeeRepository.class);
+        // employeeService = new EmployeeServiceImpl(employeeRepository);
+
+        employee1=Employee.builder()
+                .id(1L)
+                .firstName("Palash")
+                .lastName("Samanta")
+                .email("palash.samanta7497@gmail.com")
+                .build();
     }
 
     @DisplayName("save employee")
@@ -30,6 +46,8 @@ public class EmployeeServiceTests {
     public void givenEmployee_whenSaveEmployee_thenReturnEmployee(){
 
         //given- precondition or setup
+
+        /*
                   employee1=Employee.builder()
                                     .id(1L)
                                     .firstName("Palash")
@@ -37,13 +55,14 @@ public class EmployeeServiceTests {
                                     .email("palash.samanta7497@gmail.com")
                                     .build();
 
-        BDDMockito.given(employeeRepository.findByEmail(employee1.getEmail())).willReturn(Optional.empty());
+*/
+       given(employeeRepository.findByEmail(employee1.getEmail())).willReturn(Optional.empty());
 
-        BDDMockito.given(employeeRepository.save(employee1)).willReturn(employee1);
+       given(employeeRepository.save(employee1)).willReturn(employee1);
 
 
         //when- behaviour or function
-         Employee savedEmployee=employeeService.saveEmployee(employee1);
+         Employee savedEmployee=employeeServiceImpl.saveEmployee(employee1);
 
 
         //then- output
